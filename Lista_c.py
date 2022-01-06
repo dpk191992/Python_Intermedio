c4522efb754b03601a3fedc42cc740d2aa96ec6a
Daniel_Polo_Koch <dpk1992@gmail.com>
2022-01-06 23:54:18 +0100

    los archivos hasta ahora

---
+++ Lista_c.py
@@ -0,0 +1,5 @@
+lista = [1,2,3,4,5,6]
+
+lista_2 = [i**2 for i in lista]
+
+print(lista_2)
---
+++ al_cuadrado.py
@@ -0,0 +1,6 @@
+num= 1
+i = int(input("cuantos ciclos quieres?"))
+
+while num <= i:
+    print(num*num)
+    num= num+1
